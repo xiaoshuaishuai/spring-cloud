6ca90e0a41d1bcc7ca5f901f1f183e6ff7698cd8
shuaishuaixiao <xstojob@gamil.com>
2017-09-28 17:32:24 +0800

    eureka单节点注册中心

---
+++ spring-cloud-eureka/src/main/java/org/my/spring/Application.java
@@ -0,0 +1,14 @@
+package org.my.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer//开启eureka 服务
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
