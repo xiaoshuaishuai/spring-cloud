58ee74dc3162c5be00ce420db5e6759913a8ade4
shuaishuaixiao <xstojob@gamil.com>
2017-09-29 16:00:26 +0800

    测试zuul负载均衡的服务提供者

---
+++ spring-cloud-service-provider-node-01/src/main/java/org/my/spring/cloud/service/provider/HelloController.java
@@ -0,0 +1,15 @@
+package org.my.spring.cloud.service.provider;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String hello(@RequestParam String message) {
+		System.out.println("HelloController.hello()-node-01" + message);
+		return "hello:-node-01" + message;
+	}
+}
