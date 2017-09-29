b829c1eb6109fe87aa1951c0b0dc3bb4c72adac0
shuaishuaixiao <xstojob@gamil.com>
2017-09-29 16:01:11 +0800

    api网关zuul组件

---
+++ spring-cloud-zuul-gateway/src/main/java/org/my/spring/Application.java
@@ -0,0 +1,15 @@
+package org.my.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
